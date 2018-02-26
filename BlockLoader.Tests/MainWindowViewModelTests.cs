a1624c8f55d9475e1d885cb3a52fb65bc200eca5
MV <miroveith@gmail.com>
2018-02-26 15:29:23 +0100

    Sample test

--- BlockLoader.Tests/MainWindowViewModelTests.cs
+++ BlockLoader.Tests/MainWindowViewModelTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using BlockLoader.DataLayer;
 using BlockLoader.PresentationLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,23 +10,50 @@ namespace BlockLoader.Tests
 	[TestClass]
 	public class MainWindowViewModelTests
 	{
+		private IList<Block> _blocks;
+		private IList<Respondent> _respondents;
+		private MainWindowViewModel _mainWindowViewModel;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_blocks = new List<Block>();
+			_respondents = new List<Respondent>();
+
+			_mainWindowViewModel = new MainWindowViewModel(
+				new BlockRepositoryFake(_blocks),
+				new RespondentRepositoryFake(_respondents));
+
+			_blocks.Add(new Block("a", 20, "Neváhej a toč"));
+			_blocks.Add(new Block("b", 25, "Neváhej a koukej"));
+			_blocks.Add(new Block("c", 35, "Neváhej a padej"));
+		}
+
 		[TestMethod]
 		public async Task LoadBlocks_AllBlocksLoaded()
 		{
-			var blocks = new[]
-			             {
-				             new Block("a", 20, "Neváhej a toč"),
-				             new Block("b", 25, "Neváhej a koukej"),
-				             new Block("c", 35, "Neváhej a padej")
-			             };
-			var mainWindowViewModel = new MainWindowViewModel(new BlockRepositoryFake(blocks), null);
-			await mainWindowViewModel.LoadBlocks();
+			await _mainWindowViewModel.LoadBlocks();
 
-			Assert.AreEqual(blocks.Length, mainWindowViewModel.Blocks.Count);
+			Assert.AreEqual(_blocks.Count, _mainWindowViewModel.Blocks.Count);
 
-			Assert.AreEqual(blocks[0].Program, mainWindowViewModel.Blocks[0].Program);
-			Assert.AreEqual(blocks[1].Program, mainWindowViewModel.Blocks[1].Program);
-			Assert.AreEqual(blocks[2].Program, mainWindowViewModel.Blocks[2].Program);
+			Assert.AreEqual(_blocks[0].Program, _mainWindowViewModel.Blocks[0].Program);
+			Assert.AreEqual(_blocks[1].Program, _mainWindowViewModel.Blocks[1].Program);
+			Assert.AreEqual(_blocks[2].Program, _mainWindowViewModel.Blocks[2].Program);
+		}
+
+		[TestMethod]
+		public async Task CalculateReaches_UpdateReachedRespondentsCount()
+		{
+			_respondents.Add(new Respondent("1", new[] { "a", "b" }));
+			_respondents.Add(new Respondent("2", new[] { "c" }));
+			_respondents.Add(new Respondent("3", new[] { "a" }));
+
+			await _mainWindowViewModel.LoadBlocks();
+			await _mainWindowViewModel.CalculateReaches();
+
+			Assert.AreEqual(2, _mainWindowViewModel.Blocks.Single(b => b.Code == "a").ReachedRespondentsCount);
+			Assert.AreEqual(1, _mainWindowViewModel.Blocks.Single(b => b.Code == "b").ReachedRespondentsCount);
+			Assert.AreEqual(1, _mainWindowViewModel.Blocks.Single(b => b.Code == "c").ReachedRespondentsCount);
 		}
 	}
 }
---
+++ BlockLoader.Tests/RespondentRepositoryFake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using BlockLoader.DataLayer;
+
+namespace BlockLoader.Tests
+{
+	internal class RespondentRepositoryFake : IRespondentRepository
+	{
+		private readonly IList<Respondent> _respondents;
+
+		public RespondentRepositoryFake(IList<Respondent> respondents)
+		{
+			_respondents = respondents;
+		}
+
+		public IEnumerable<Respondent> LoadRespondents()
+		{
+			return _respondents;
+		}
+	}
+}
